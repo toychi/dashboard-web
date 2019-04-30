282163b0ba96ce72453c087dbd8f50eebd93448e
pppraewkyl <ppraewkyl@hotmail.com>
2019-04-30 13:56:26 +0700

    Source page only front-end

--- src/pages/Sources.vue
+++ src/pages/Sources.vue
@@ -1,106 +1,92 @@
 <template>
-    <div class="row">
-      <div class="col-12">
-        <card :title="table1.title" :subTitle="table1.subTitle">
-          <div slot="raw-content" class="table-responsive">
-            <paper-table :data="table1.data" :columns="table1.columns" :columnsKey="table1.columnsKey">
+<div>
+ 
+  <br>
+  <card>
+   <table class="table table-bordered table-hover">
 
-            </paper-table>
-          </div>
-        </card>
-      </div>
-
+  <thead>
+    <tr>
+      <th scope="col"></th>
+      <th scope="col">#</th>
+      <th scope="col">Website</th>
+      <th scope="col">Status</th>
+      <th scope="col">Last crawling</th>
+      <th scope="col">Last down</th>
+      <th scope="col">Number of records</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><input type="checkbox" name="name1" /></td>
+      <th scope="row">1</th>
+      <td>Thaihometown</td>
+      <td class="table-success" >Crawling</td>
+      <td>2018-10-02</td>
+      <td>-</td>
+      <td>7542</td>
+    </tr>
+    <tr>
+      <td><input type="checkbox" name="name2" /></td>
+      <th scope="row">2</th>
+      <td>Thaiger Property</td>
+      <td class="table-success">Crawling</td>
+      <td>2018-11-12</td>
+      <td>-</td>
+      <td>8976</td>
+    </tr>
+    <tr>
+      <td><input type="checkbox" name="name3" /></td>
+      <th scope="row">3</th>
+      <td>Homefinder</td>
+      <td class="table-danger">Stop</td>
+      <td>-</td>
+      <td>2018-11-12</td>
+      <td>5976</td>
+    </tr>
+  </tbody>
+</table>
+</card>
 
 <div class="upload-btn-wrapper col-3">
   <h4>User Input </h4>
   <button class="btn">Upload a file</button>
   <input type="file" name="myfile" />
 </div>
+<br>
 
-      <div class="col-12">
-        <card class="card-plain">
-          <div class="table-full-width table-responsive">
-            <paper-table type="hover" :title="table2.title" :sub-title="table2.subTitle" :data="table2.data"
-                         :columns="table2.columns" :columnsKey="table2.columnsKey">
-
-            </paper-table>
-          </div>
-        </card>
-      </div>
-
-    </div>
+<card>
+   <table class="table table-bordered table-hover">
+     
+  <thead>
+    <tr>
+      <th scope="col"></th>
+      <th scope="col">#</th>
+      <th scope="col">File</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><input type="checkbox" name="name1" /></td>
+      <th scope="row">1</th>
+      <td>Sources1</td>
+      <td class="td-actions text-left">
+        <button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-xs">
+        <i class="fa fa-times"></i>
+        </button></td>
+  
+    </tr>
+    </tbody>
+  </table>
+  </card>
+</div>      
 </template>
 <script>
-import { PaperTable } from "@/components";
-const tableColumns = ["#", "Website", "Status", "Last Crawling", "Last Down"];
-const tableColumnsKey = ["id", "website", "status", "lastCrawling", "lastDown"];
-const tableData = [
-  {
-    id: 1,
-    website: "Thaihometown.com",
-    status: "Crawling",
-    lastCrawling: "2018-10-02",
-    lastDown: "-"
-  },
-  {
-    id: 2,
-    website: "Bannia.com",
-    status: "Crawling",
-    lastCrawling: "2018-10-02",
-    lastDown: "2017-10-02"
-  },
-  {
-    id: 3,
-    website: "Source1",
-    status: "Stop",
-    lastCrawling: "2018-09-03",
-    lastDown: "-"
-  },
-  {
-    id: 4,
-    website: "Source2",
-    status: "Stop",
-    lastCrawling: "2018-10-12",
-    lastDown: "2018-10-04"
-  }
-];
 
-const tableColumns2 = ["#", "Name", "Action"];
-const tableColumnsKey2 = ["id", "name", "action"];
-const tableData2 = [
-  {
-    id: 1,
-   name: "source1",
-   action: "Edit"
-
-  }
-];
-
-export default {
-  components: {
-    PaperTable
-  },
-  data() {
-    return {
-      table1: {
-        title: "Crawling Data",
-        subTitle: "",
-        columns: [...tableColumns],
-        data: [...tableData],
-        columnsKey: [...tableColumnsKey]
-      },
-      table2: {
-        title: "User Input",
-        subTitle: "",
-        columns: [...tableColumns2],
-        data: [...tableData2],
-        columnsKey: [...tableColumnsKey2]
-      }
-    };
-  }
-};
 </script>
 <style>
+
 .upload-btn-wrapper {
   position: relative;
   overflow: hidden;
@@ -125,3 +111,4 @@ export default {
   opacity: 0;
 }
 </style>
+
