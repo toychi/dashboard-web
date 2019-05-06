ff0f056d8bec352210113602dd72d7daa1d81def
Pcr <stammer.pachara@gmail.com>
2019-05-06 23:03:32 +0700

    upload file

--- src/pages/Sources.vue
+++ src/pages/Sources.vue
@@ -54,8 +54,14 @@
 
 <div class="upload-btn-wrapper col-3">
   <h4>User Input </h4>
-  <button class="btn">Upload a file</button>
-  <input type="file" name="myfile" />
+  <div class="container">
+    <div class="large-12 medium-12 small-12 cell">
+      <label>File
+        <input type="file" id="file" ref="file" v-on:change="handleFileUpload()"/>
+      </label>
+      <button v-on:click="submitFile()">Submit</button>
+    </div>
+  </div>
 </div>
 <br>
 
@@ -86,9 +92,63 @@
   </card>
 </div>      
 </template>
+<script>
+import axios from "axios";
 
+export default {
+    /*
+      Defines the data used by the component
+    */
+    data(){
+      return {
+        file: ''
+      }
+    },
+
+    methods: {
+      /*
+        Submits the file to the server
+      */
+      submitFile(){
+        /*
+                Initialize the form data
+            */
+            let formData = new FormData();
+
+            /*
+                Add the form data we need to submit
+            */
+            formData.append('file', this.file);
+
+        /*
+          Make the request to the POST /single-file URL
+        */
+            axios.post('http://35.187.253.51:4000/uploadfile',
+                formData,
+                {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+              }
+            ).then(function(){
+          console.log('SUCCESS!!');
+        })
+        .catch(function(){
+          console.log('FAILURE!!');
+        });
+      },
+
+      /*
+        Handles a change on the file upload
+      */
+      handleFileUpload(){
+        this.file = this.$refs.file.files[0];
+      }
+    }
+  }
+</script>
 <style>
-.upload-btn-wrapper {
+/*.upload-btn-wrapper {
   position: relative;
   overflow: hidden;
   display: inline-block;
@@ -110,6 +170,6 @@
   left: 0;
   top: 0;
   opacity: 0;
-}
+}*/
 </style>
 
